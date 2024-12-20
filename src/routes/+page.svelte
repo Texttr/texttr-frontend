050decc02e7f47f977cb78799013be76c5075790
constt <mattheo.hering@gmail.com>
2024-12-20 23:33:19 +0100

    :sparkles: Added a Text area

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<div>
-  <textarea class="overflow-hidden text-white bg-transparent w-full h-screen border-none outline-none"></textarea>
+<div class="box-border overflow-hidden p-0 m-0">
+  <textarea 
+    class="pl-5 pt-5 font-jbmono overflow-hidden text-white bg-transparent w-screen h-screen border-none outline-none" ></textarea>
 </div>
